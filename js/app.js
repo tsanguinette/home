66abf0c0d03d99f70353b1cd727b86c072e26ddd
Tyler Sanguinette <hello@tylersanguinette.com>
2017-06-12 18:08:35 -0500

    Yearly push.

--- js/app.js
+++ js/app.js
@@ -11,3 +11,14 @@ $("#shareRoundIcons").jsSocials({
     showCount: false,
     shares: ["email", "twitter", "facebook", "googleplus", "linkedin", "pinterest", "whatsapp"]
 });
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a.simple-scroll").on('click', function(event) {
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({scrollTop: $('#projects').offset().top-50}, 800);
+	  return false;
+  });
+});
